188063a0aa8fd8374b68b7b2a6931d317523b60e
Guillermo Cabrera <guicaro@gmail.com>
2017-04-16 16:49:44 +0300

    Fixed minor issue with start/stop scripts

--- start-all.sh
+++ start-all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash          
 
 cd ~/kafka-docker && sudo docker-compose up -d
-cd ~/docker-hadoop-spark-workbench && \ 
-   sudo docker-compose up -d
+cd ~/docker-hadoop-spark-workbench && sudo docker-compose up -d
--- stop-all.sh
+++ stop-all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash          
 
 cd ~/kafka-docker && sudo docker-compose stop
-cd ~/docker-hadoop-spark-workbench && \ 
-   sudo docker-compose stop
+cd ~/docker-hadoop-spark-workbench && sudo docker-compose stop
