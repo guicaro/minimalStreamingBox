5e56288f9164b6de809f014fa76f567d863166cf
Guillermo Cabrera <guicaro@gmail.com>
2017-04-17 14:59:44 +0300

    Removed Kafka env vars, they go in docker-compose.yml

--- start-all.sh
+++ start-all.sh
@@ -1,7 +1,4 @@
 #!/bin/bash          
 
-export KAFKA_HEAP_OPTS="-Xmx512M -Xms128M"
-export KAFKA_ADVERTISED_HOST_NAME=localhost
-
 cd ~/kafka-docker && sudo docker-compose up -d
 cd ~/docker-hadoop-spark-workbench && sudo docker-compose -f docker-compose-java8.yml up -d
