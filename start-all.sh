222481f207c0a001282a20d458079323bff1acb0
Guillermo Cabrera <guicaro@gmail.com>
2017-04-17 08:02:04 +0300

    Using different docker-compose file with Spark 2.1

--- start-all.sh
+++ start-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash          
 
 cd ~/kafka-docker && sudo docker-compose up -d
-cd ~/docker-hadoop-spark-workbench && sudo docker-compose up -d
+cd ~/docker-hadoop-spark-workbench && sudo docker-compose -f docker-compose-java8.yml up -d
