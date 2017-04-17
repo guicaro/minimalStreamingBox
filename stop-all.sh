8a469dcde5ab9ffe9038f76382a674c8559d59dd
Guillermo Cabrera <guicaro@gmail.com>
2017-04-17 12:58:33 +0300

    Specified the docker-compose file we are using

--- stop-all.sh
+++ stop-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash          
 
 cd ~/kafka-docker && sudo docker-compose stop
-cd ~/docker-hadoop-spark-workbench && sudo docker-compose stop
+cd ~/docker-hadoop-spark-workbench && sudo docker-compose -f docker-compose-java8.yml stop
